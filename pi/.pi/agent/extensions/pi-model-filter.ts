e2570f983e77d0867c43b718c31f6dba4f33d675
Nick Muoh <nick_muoh@trimble.com>
2026-08-19 16:49:43 -0700

    Moved custom extensions to dedicated folder

--- pi/.pi/agent/extensions/pi-model-filter.ts
+++ pi/.pi/agent/extensions/pi-model-filter.ts
@@ -1,1 +1,1 @@
-export { default } from "../../../pi-model-filter/src/extension.ts";
+export { default } from "../custom_extensions/pi-model-filter/src/extension.ts";
--- pi/.pi/agent/extensions/pi-tmux-agent-indicator.ts
+++ pi/.pi/agent/extensions/pi-tmux-agent-indicator.ts
@@ -1,1 +1,1 @@
-export { default } from "../../../pi-tmux-agent-indicator/src/extension.ts";
+export { default } from "../custom_extensions/pi-tmux-agent-indicator/src/extension.ts";
